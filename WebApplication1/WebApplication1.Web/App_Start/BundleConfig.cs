02478fbf7d1e99c3a4b146ffdcd2013886180c4f
slivaburaga <slivaburaga@gmail.com>
2024-04-24 21:14:11 +0300

    Initial commit

---
+++ WebApplication1/WebApplication1.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace WebApplication1
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                "~/css/bootstrap.min.css",
+                "~/css/style.css",
+                "~/css/responsive.css",
+                "~/css/jquery.mCustomScrollbar.min.css"
+            ));
+            
+            bundles.Add(new StyleBundle("~/bundles/css2").Include(
+                "~/css/owl.carousel.min.css",
+                "~/css/owl.theme.default.min.css",
+                "~/css/jquery.fancybox.min.css"
+            ));
+        }
+    }
+}
