d121bc838bc0d8e16ed2a92a549d594c7d543482
sasha.gajdarly <sasha.gajdarly@mail.ru>
2024-05-06 18:02:09 +0300

    add db context

---
+++ WebApplication1/WebApplication1.Database/DbContext.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.Entity;
+using WebApplication1.Domain;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<ContactForm> ContactForms { get; set; }
+    public ApplicationDbContext() : base("DefaultConnection")
+    {
+    }
+
+    protected override void OnModelCreating(DbModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>()
+            .HasRequired(p => p.Category)
+            .WithMany(c => c.Products)
+            .HasForeignKey(p => p.CategoryId);
+
+        modelBuilder.Entity<OrderItem>()
+            .HasRequired(oi => oi.Order)
+            .WithMany(o => o.OrderItems)
+            .HasForeignKey(oi => oi.OrderId);
+
+        modelBuilder.Entity<OrderItem>()
+            .HasRequired(oi => oi.Product)
+            .WithMany(p => p.OrderItems)
+            .HasForeignKey(oi => oi.ProductId);
+        
+        base.OnModelCreating(modelBuilder);
+    }
+}
