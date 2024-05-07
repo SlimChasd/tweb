cb87d2777d17495d0f57cdb2e9c42fa4597ed052
sasha.gajdarly <sasha.gajdarly@mail.ru>
2024-05-07 13:38:08 +0300

    Add admin controller

---
+++ WebApplication1/WebApplication1.Web/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _context = new ApplicationDbContext();
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var contactForms = _context.ContactForms.ToList();
+            return View(contactForms);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _context.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
